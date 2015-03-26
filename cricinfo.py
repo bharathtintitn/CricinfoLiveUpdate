1a5a1f17dd7bffe2c6312ae6ac9ebe186eacf649
Bharath <bharath@ubuntu.ubuntu-domain>
2015-03-25 21:38:49 -0700

    keyboard interrupt added

--- cricinfo.py
+++ cricinfo.py
@@ -13,13 +13,24 @@ def sendmessage(title, message):
     notice.show()
     return
 url = "http://static.cricinfo.com/rss/livescores.xml"
+match, interrupted = "", False
 while True:
     r = requests.get(url)
-    while r.status_code is not 200:
-            r = requests.get(url)
-    soup = BeautifulSoup(r.text)
-    data = soup.find_all("description")
-    score = data[2].text
-    sendmessage("Score", score)
-    sleep(60)
+    try:
+	    while r.status_code is not 200:
+		    r = requests.get(url)
+	    soup = BeautifulSoup(r.text)
+	    data = soup.find_all("description")
+	    score = data[1].text
+	    sendmessage("Score", score)
+	    sleep(60)
+
+    except KeyboardInterrupt:
+
+	if interrupted:
+		print "bye bye"
+		break
+	else:
+		print "Press ctrl+c again"
+		match, interrupted = 0, True
 
